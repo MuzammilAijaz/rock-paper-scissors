e206eb3879163147fcc09eae0b0227e065692dd2
Muzammil <171587778+MuzammilAijaz@users.noreply.github.com>
2025-04-22 17:19:58 +0500

    add logic and all basic functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# Plan
+- output will be  rock, paper, or scissors
+- input will be from user : rock, paper, scissors
+
+# Pseudocode
+INPUT : user enters choice
+generate a random num, from 1 to 3
+1 = rock
+2 = paper
+3 = scissors 
+check who won, rock beats scissors, paper beats......
--- resources/test.js
+++ resources/test.js
@@ -1,1 +1,50 @@
-console.log("Hello world");
+function getComputerChoice(){
+    let comp_response = Math.floor(Math.random()*3) + 1;
+    switch(comp_response){
+        case 1:
+            comp_response = "rock";
+            break;
+        case 2:
+            comp_response = "paper"
+            break;
+        case 3:
+            comp_response = "scissors"
+            break;
+    }
+    console.log("Computer picked " + comp_response)
+    return comp_response;
+}
+function getHumanChoice(){
+    while(1){
+        let response = prompt("Rock, Paper or Scissors");
+        if(!response) {response = response.toLowerCase();}
+        const possibleChoices = ["rock","paper","scissors"];
+        if (possibleChoices.includes(response)){
+            console.log(`You picked ${response}`);
+            return response;
+        }
+        console.log("Invalid response entered: retry");
+    }
+}
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === 'rock' && computerChoice === 'scissors'||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice == 'paper'
+    ) {return 1;}
+    else if (humanChoice === computerChoice) {return 0;}
+    else {return -1;}
+}
+let humanScore = 0 ;
+let computerScore = 0;
+
+while(1){
+    console.log("score: " + humanScore);
+    console.log("Enter your choice : (1) = Rock, (2) = Paper, (3) = Scissors");
+    let humanChoice = getHumanChoice();
+
+    let computerChoice = getComputerChoice();
+    let win = playRound(humanChoice, computerChoice);
+    if(win === -1){console.log("You lost"); --humanScore;++computerScore}
+    else if(win === 1) {console.log("You won"); ++humanScore; --computerScore}
+    else {console.log("Its a draw");}
+}
