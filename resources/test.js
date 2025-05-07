17a27c78f29fe1e15441387cb01d58dc96cabd68
Muzammil <171587778+MuzammilAijaz@users.noreply.github.com>
2025-05-07 06:30:37 +0500

    add UI

--- resources/test.js
+++ resources/test.js
@@ -37,19 +37,62 @@ function playRound(humanChoice, computerChoice){
 let humanScore = 0 ;
 let computerScore = 0;
 
-function playGame(){
-    console.log("score: " + humanScore);
-    console.log("Enter your choice : (1) = Rock, (2) = Paper, (3) = Scissors");
-    let humanChoice = getHumanChoice();
 
-    let computerChoice = getComputerChoice();
-    let win = playRound(humanChoice, computerChoice);
-    if(win === -1){console.log("You lost"); --humanScore;++computerScore}
-    else if(win === 1) {console.log("You won"); ++humanScore; --computerScore}
-    else {console.log("Its a draw");}
+// function playGame(){
+//     console.log("score: " + humanScore);
+//     console.log("Enter your choice : (1) = Rock, (2) = Paper, (3) = Scissors");
+//     let humanChoice = getHumanChoice();
+
+//     let computerChoice = getComputerChoice();
+//     let win = playRound(humanChoice, computerChoice);
+//     if(win === -1){console.log("You lost"); --humanScore;++computerScore}
+//     else if(win === 1) {console.log("You won"); ++humanScore; --computerScore}
+//     else {console.log("Its a draw");}
+// }
+
+
+
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
+
+let selections = document.querySelector('.selections');
+let displayHuman = document.querySelector('#humanScore');
+let displayComputer = document.querySelector('#computerScore');
+
+selections.addEventListener('click', (event) => {
+    let target = event.target;
+
+    let ComputerChoice = getComputerChoice();
+    let result = 0;
+    switch(target.id){
+        case 'rock':
+            result = playRound('rock', ComputerChoice)
+            console.log("u pressed rock");
+            break;
+        case 'paper':
+            result = playRound('paper', ComputerChoice)
+            console.log("you pressed paper");
+            break;
+        case 'scissors':
+            result = playRound('scissors', ComputerChoice)
+            console.log("you pressed scissors");
+            break;
+    }
+    updateScore(result);
+    if(humanScore == 5){
+        window.alert("You have WON!!");
+    }
+    else if(computerScore == 5 ){
+        window.alert("You have lost :(")
+    }
+})
+
+function updateScore(val){
+    humanScore += val;
+    computerScore -= val;
+
+    displayComputer.textContent = computerScore;
+    displayHuman.textContent = humanScore;
 }
 
-let PLAYTIMES = 5;
-for(let i = 0; i < PLAYTIMES ;i++){
-    playGame();
-}
